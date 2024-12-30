8d65a9008bb57ff1c5dd8c68781e57a1c34a791d
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-12-31 02:30:21 +0300

    Feat(task-flow-frontend): Add BaseInput component and integrate it into LoginPage; update styles for form layout

---
+++ task-flow-frontend/src/components/BaseInput.tsx
@@ -0,0 +1,7 @@
+import classes from "./BaseInput.module.scss";
+
+export default function BaseInput() {
+    return <div className={classes.formController}>
+        fd
+    </div>
+}
--- task-flow-frontend/src/pages/auth/LoginPage.tsx
+++ task-flow-frontend/src/pages/auth/LoginPage.tsx
@@ -1,5 +1,6 @@
 import classes from './LoginPage.module.scss';
 import AuthPageContainer from '../../components/AuthPageContainer';
+import BaseInput from '../../components/BaseInput';
 
 export default function LoginPage() {
     return (
@@ -8,6 +9,13 @@ export default function LoginPage() {
             <div className={classes.welcomeText}>
                 <p>Please enter your details</p>
                 <h1>Welcome back</h1>
+            </div>
+
+            <div className={classes.form} >
+                fds
+                <form action="">
+                    <BaseInput />
+                </form>
             </div>
 
 
