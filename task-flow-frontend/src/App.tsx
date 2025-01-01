8fe2803f72aacfb42fddccdbd03e95121ff8c2d5
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2025-01-01 13:48:07 +0300

    Feat(App): Implement auto-login functionality on application startup

--- task-flow-frontend/src/App.tsx
+++ task-flow-frontend/src/App.tsx
@@ -1,7 +1,13 @@
 import { useRoutes } from 'react-router';
 import { appRoutes } from './router/routes';
+import { useDispatch } from 'react-redux';
+import { autoLogin } from './redux/slices/authSlice';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  dispatch(autoLogin());
+
   const routes = useRoutes(appRoutes)
   return <>
     {routes}
